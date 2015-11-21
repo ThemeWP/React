43f56d7d72fb87821fdc4f4dffe4c7ddaa3e9236
ThemeWP <info@themewp.nl>
2015-11-21 12:46:28 +0100

    first commit

---
+++ app/hello-world.jsx
@@ -0,0 +1,15 @@
+// hello-world.jsx
+
+import React from 'react';
+
+class HelloWorld extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    )
+  }
+}
+
+export default HelloWorld;
